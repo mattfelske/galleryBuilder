2c3dfafd3a1bd98cd5935e4e7273d5a62d49ca53
Matt Felske <mattfelske@yahoo.com>
2014-03-26 10:16:40 -0400

    version 0.0.1

--- js/galleryBuilder.js
+++ js/galleryBuilder.js
@@ -1,23 +1,47 @@
-function galleryBuilder() {
+/**
+ * The galleryBuilder tool is designed to allow a developer to a vertical or horizontal gallery 
+ * that has been sorted into buckets of fixed element length. The elements can be custom tailored
+ * to include whatever format the user requires.
+ * 
+ * Currently Supported Formats
+ *  - String
+ *  
+ * SETUP INSTRUCTIONS ******************************************
+ * 
+ * 1. 	Create a variable and create a new galleryBuilder object. 
+ * 		Pass in the ID of the container that the gallery will be located in.
+ * 
+ * 		myApp=new galleryBuilder();
+ * 		myApp.initializeApp();
+ * 
+ * @author Matt Felske
+ * @version 1.0
+ */
+
+function galleryBuilder(containerID) {
 
 	this.initializeApp=initializeApp;
-	function initializeApp() {
+	function initializeApp(id) {
 		console.log("Initializing ...");
 	    
 	    buildBuckets()
-	    buildGallery("myContainer");
+	    buildGallery(containerID);
 	    
-	    $(function(){
-	        $(".gb-scrollX").scroll(function(){
-	            $(".gb-container")
-	                .scrollLeft($(".gb-scrollX").scrollLeft());
-	        });
-	        
-	        $(".gb-scrollY").scroll(function(){
-	            $(".gb-container")
-	                .scrollTop($(".gb-scrollY").scrollTop());
-	        });
-	    });
+	    var axisY = document.getElementById("axisY");
+	    var axisX = document.getElementById("axisX");
+	    var gbContainer = document.getElementById("gb-container");
+	    if(axisY != null) {
+	    	axisY.onscroll = function (event) {
+	    		//console.log("scroll event detected! " + axisY.scrollTop);
+	    		gbContainer.scrollTop = axisY.scrollTop;
+	    	}
+	    }
+	    if(axisX != null) {
+	    	axisX.onscroll = function (event) {
+	    		//console.log("scroll event detected! " + axisX.scrollTop);
+	    		gbContainer.scrollLeft = axisX.scrollLeft;
+	    	}
+	    }
 	}
 	
 	
@@ -25,25 +49,25 @@ function galleryBuilder() {
 	var SCROLL_VERTICAL = 1;
 	
 	/**** USER SUBMITTED *********************************/
-	var scrollAlignment = SCROLL_VERTICAL;
+	var scrollAlignment = SCROLL_HORIZONTAL;
 	var numberPerBuckets = 5;
 	var bucketElementWidth = 42;
 	var bucketElementHeight = 62;
 	var bucketElementMarginTop = 10;
-	var bucketElementMarginRight = 10;
+	var bucketElementMarginRight = 15;
 	var bucketElementMarginBottom = 10;
-	var bucketElementMarginLeft = 10;
+	var bucketElementMarginLeft = 15;
 	/*****************************************************/
 	
 	var data = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"];
 	var buckets = new Array();
 	
-	var gbContainer = document.getElementById("myContainer");
+	var gbContainer = document.getElementById(containerID);
 	
-	var surfaceWidth = document.getElementById("myContainer").offsetWidth;
-	var surfaceHeight = document.getElementById("myContainer").offsetHeight;
-	var windowOffsetWidth = document.getElementById("myContainer").offsetLeft;
-	var windowOffsetHeight = document.getElementById("myContainer").offsetTop;
+	var surfaceWidth = document.getElementById(containerID).offsetWidth;
+	var surfaceHeight = document.getElementById(containerID).offsetHeight;
+	var windowOffsetWidth = document.getElementById(containerID).offsetLeft;
+	var windowOffsetHeight = document.getElementById(containerID).offsetTop;
 	
 	//initializeApp();
 	
@@ -59,18 +83,28 @@ function galleryBuilder() {
 		
 		var galleryNode = document.createElement("DIV");
 		var att = document.createAttribute("class");
+		var att2 = document.createAttribute("style");
+		var att3 = document.createAttribute("id");
 		att.value="gb-container";
+		att3.value="gb-container";
+		if(scrollAlignment == SCROLL_HORIZONTAL) {
+			att2.value="left: 0px; top: 20px;";
+		} else {
+			att2.value="left: 20px; top: 0px;";
+		}	
 		galleryNode.setAttributeNode(att);
+		galleryNode.setAttributeNode(att2);
+		galleryNode.setAttributeNode(att3);
 		
 		var sumHeight = 0;
 		var sumWidth = 0;
 		if(scrollAlignment == SCROLL_HORIZONTAL) {
-			sumHeight = (bucketElementHeight + bucketElementMarginTop +bucketElementMarginBottom) * numberPerBuckets;
-			sumWidth = bucketElementWidth + ((bucketElementMarginTop + bucketElementMarginBottom ) *2);
+			sumHeight = (bucketElementHeight * numberPerBuckets) + (bucketElementMarginBottom * (numberPerBuckets - 1));
+			sumWidth = bucketElementWidth + bucketElementMarginLeft + bucketElementMarginRight;
 	
 		} else {
 			sumHeight = bucketElementHeight + bucketElementMarginTop + bucketElementMarginBottom;
-			sumWidth = (bucketElementWidth + bucketElementMarginLeft + bucketElementMarginRight) * numberPerBuckets;
+			sumWidth = (bucketElementWidth * numberPerBuckets) + (bucketElementMarginRight * (numberPerBuckets - 1));
 		}	
 	
 		
@@ -103,7 +137,7 @@ function galleryBuilder() {
 			sum = (elementHeight * buckets.length) + ((buckets.length - 1) * elementMarginBottom) + elementMarginBottom;
 		}	
 		
-		generateScrollbars(galleryNode, sum);
+		generateScrollbars(gbContainer, sum);
 		
 	}
 	
@@ -117,11 +151,13 @@ function galleryBuilder() {
 		if(scrollAlignment == SCROLL_HORIZONTAL) {
 			att1.value="gb-scrollX";
 			att2.value="axisX";
-			att3.value="left: "+windowOffsetWidth+"px; top: "+(windowOffsetHeight - 20)+"px; width:"+surfaceWidth+"px;";
+			//att3.value="left: "+windowOffsetWidth+"px; top: "+(windowOffsetHeight - 20)+"px; width:"+surfaceWidth+"px;";
+			att3.value="width:"+surfaceWidth+"px; top:-"+surfaceHeight+"px;";
 		} else {
 			att1.value="gb-scrollY";
 			att2.value="axisY";
-			att3.value="left: "+(windowOffsetWidth - 20)+"px; top: "+windowOffsetHeight+"px; height:"+surfaceHeight+"px;";
+			//att3.value="left: "+(windowOffsetWidth - 20)+"px; top: "+windowOffsetHeight+"px; height:"+surfaceHeight+"px;";
+			att3.value="height:"+surfaceHeight+"px; top:-"+surfaceHeight+"px;";
 		}		
 		node.setAttributeNode(att1);
 		node.setAttributeNode(att2);
@@ -193,8 +229,8 @@ function galleryBuilder() {
 
 }
 
-myApp=new galleryBuilder();
-myApp.initializeApp();
+/*myApp=new galleryBuilder();
+myApp.initializeApp();*/
 
 
 
